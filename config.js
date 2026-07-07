2a7ae278622632530ce169ceba786b0d08779347
FARISCO-Ltd <149786411+FARISCO-Ltd@users.noreply.github.com>
2026-07-07 11:03:16 +0400

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const PROFILE = {
   website: "https://farisco-ltd.github.io",
   links: {
     creators: "https://creators.sa/farisco1218",
-    paypal: "https://www.paypal.com/paypalme/yourname",
-    tiktok: "https://www.tiktok.com/@yourusername",
-    telegram: "https://t.me/yourusername"
+    paypal: "https://www.paypal.com/paypalme/farisco1218",
+    tiktok: "https://www.tiktok.com/@farisco.ltd",
+    telegram: "https://t.me/faris_ltd"
   },
   labels: {
     en: {
